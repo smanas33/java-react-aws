1e3b13e897c12fe378dfa09fa9ee0b8961328ec0
smanas33 <sahoo.manasranjan0@gmail.com>
2023-03-25 23:05:14 +0530

    Arima: Initial Commit

---
+++ src/main/java/com/arima/javareactaws/JavaReactAwsApplication.java
@@ -0,0 +1,13 @@
+package com.arima.javareactaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReactAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arima/javareactaws/JavaReactAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arima.javareactaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReactAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
